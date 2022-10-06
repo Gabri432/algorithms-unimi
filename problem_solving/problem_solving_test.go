392a4839ec7f6d90cec49fece3561a03083544d7
Gabri432 <gabrielegatti432@gmail.com>
2022-10-06 22:35:43 +0200

    Improving tests clarity.

--- problem_solving/problem_solving_test.go
+++ problem_solving/problem_solving_test.go
@@ -3,9 +3,18 @@ package algorithmsunimi
 import "testing"
 
 func TestEx3(t *testing.T) {
-	t.Fatal(ex3())
+	intList := ex3()
+	expectedResult := []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
+	for i := len(expectedResult); i > 0; i-- {
+		if intList[len(expectedResult)-i] != i {
+			t.Fatalf("Expected %d, got %d", i, intList[i])
+		}
+	}
 }
 
 func TestEx4(t *testing.T) {
-	t.Fatal(ex4())
+	timesToTheLeft := ex4()
+	if timesToTheLeft != 2 {
+		t.Fatalf("Expected 2, got %d", timesToTheLeft)
+	}
 }
